923732abb529d72ca5bca0933a373552b9bdeed6
Chris Grant <cgrant1995@me.com>
2016-03-20 13:44:57 -0400

    Sunday, March 20, 2016 at 1:44:57 PM

--- Project3/src/VMSimulator.java
+++ Project3/src/VMSimulator.java
@@ -1,21 +1,56 @@
 /**
  * Created by christophergrant on 3/20/16.
  */
+import java.util.ArrayList;
+import java.util.Scanner;
 public class VMSimulator {
     public VMSimulator(){
 
     }
 
     public void opt(int frames, String tracefile){
-        System.out.println("Test");
+        int memaccess =0;
+        int pageFaults = 0;
+        int writes =0;
+        int[] pageFrames = new int[frames];
+
+
     }
     public void clock(int frames, String tracefile){
+        int memaccess =0;
+        int pageFaults = 0;
+        int writes =0;
+        int[] pageFrames = new int[frames];
+        ArrayList<PTE> pageTable = new ArrayList<>();
+        Scanner scan = new Scanner(tracefile);
 
+        int hands = 0;
+        System.out.println("Creating pageTable");
+        for(int i =0; i< 1024*1024; i++){
+            PTE temp = new PTE();
+            pageTable.add(i,temp);
+        }
+        for(int i = 0; i<frames; i++){
+            pageFrames[i] = -1;
+        }
+        int cur = 0; // current frame
+        while (scan.hasNext()){
+            String[] line = scan.nextLine().split(" ");
+            int pageNum = Integer.decode("0x" + line[0]);
+            PTE entry = pageTable.get(pageNum);
+        }
+        System.out.println("Done");
     }
     public void nru (int frames, String tracefile, int refresh){
-
+        int memaccess =0;
+        int pageFaults = 0;
+        int writes =0;
+        int[] pageFrames = new int[frames];
     }
     public void aging(int frames, String tracefile, int refresh){
-
+        int memaccess =0;
+        int pageFaults = 0;
+        int writes =0;
+        int[] pageFrames = new int[frames];
     }
 }
