745c0db99a657275277fcde966ac61e240c2e75a
Chris Grant <crg50@pitt.edu>
2016-03-23 17:01:26 -0400

    NRU

--- Project3/src/VMSimulator.java
+++ Project3/src/VMSimulator.java
@@ -104,11 +104,70 @@ public class VMSimulator {
         System.out.println("Total Writes to Disk "+ writes);
         //System.out.println("Done");
     }
-    public void nru (int frames, String tracefile, int refresh){
+    public void nru (int frames, String tracefile, int refresh)throws FileNotFoundException{
         int memaccess =0;
         int pageFaults = 0;
         int writes =0;
         int[] pageFrames = new int[frames];
+        HashMap<Integer,PTE> pageTable = new HashMap<>();
+
+        System.out.println("Creating Page Table");
+        for (int i =0; i< 1024*1024){
+            PTE temp = new PTE();
+            pageTable.put(i,temp);
+        }
+        for(int i =0; i<frames; i++){
+            pageFrames[i] = -1;
+        }
+        int cur =0;
+        Scanner scan = new Scanner(new File(tracefile));
+        while(scan.hasNext()){
+            if(memaccess % refresh == 0){
+                for(int i =0; i< cur; i++){
+                    PTE temp = pageTable.get(pageFrames[i]);
+                    temp.reference = false;
+                    pageTable.put(temp.i, temp);
+                }
+            }
+            String [] line = scan.nextLine().split(" ");
+            int pageNum = Integer.decode("0x" + line[0].substring(0,5));
+            PTE entry = pageTable.get(pageNum);
+            entry.i = pageNum;
+            entry.reference = true;
+            if (line[1].equals("W")){
+                entry.dirty = true;
+            }
+            if(!entry.v){
+                pageFaults++;
+                if(cur < frames){
+                    pageFrames[cur] = pageNum;
+                    entry.frame = cur;
+                    entry.v = true;
+                    cur++;
+                }
+                else{
+                    int pageToEvict = 0;
+                    boolean found = false;
+                    while(!found){
+                        for(int i =0; i<frames; i++){
+                            PTE temp = pageTable.get(pageFrames[i]);
+                            if(!temp.reference && !temp.dirty && temp.v){
+                                entry.frame = temp.frame;
+                                pageFrames[entry.frame] = entry.i;
+                                temp.v = false;
+                                temp.dirty = false;
+                                temp.reference = false;
+                                temp.frame = -1;
+                                pageTable.put(temp.i, temp);
+                                entry.v = true;
+                                pageTable.put(entry.i, entry);
+                                found = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     public void aging(int frames, String tracefile, int refresh){
         int memaccess =0;
