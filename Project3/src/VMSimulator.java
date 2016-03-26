609e19da81368fbd177beb6b3814dd50fda526ee
Chris Grant <cgrant1995@me.com>
2016-03-26 13:14:01 -0400

    Saturday, March 26, 2016 at 1:14:01 PM

--- Project3/src/VMSimulator.java
+++ Project3/src/VMSimulator.java
@@ -291,10 +291,68 @@ public class VMSimulator {
         //Ideal seems to be about 75
 
     }
-    public void aging(int frames, String tracefile, int refresh){
-        int memaccess =0;
+    public void aging(int frames, String tracefile, int refresh) throws FileNotFoundException {
+        int memaccess = 0;
         int pageFaults = 0;
-        int writes =0;
+        int writes = 0;
         int[] pageFrames = new int[frames];
+        Hashtable<Integer, PTE> pageTable = new Hashtable<>();
+        byte[] history = new byte[frames];
+        System.out.println("Creating Page Table");
+        for (int i = 0; i < 1024 * 1024; i++) {
+            PTE temp = new PTE();
+            pageTable.put(i, temp);
+        }
+        for (int i = 0; i < frames; i++) {
+            pageFrames[i] = -1;
+            history[i] = 0;
+        }
+        int cur = 0;
+        Scanner scan = new Scanner(new File(tracefile));
+        while (scan.hasNext()) {
+            if (memaccess % refresh == 0) {
+                for (int i = 0; i < cur; i++) {
+                    PTE temp = pageTable.get(pageFrames[i]);
+                    int t = history[i] >> 1;
+                    history[i] = (byte) t; //Why the hell do I need to do it like this java
+                    if (temp.reference) {
+                        history[i] += 128;
+                    }
+                    temp.reference = false;
+                    pageTable.put(temp.i, temp);
+                    System.out.println(history[i]);
+
+                }
+                return; // Temp Obviously
+            }
+            String[] line = scan.nextLine().split(" ");
+            int pageNum = Integer.decode("0x" + line[0].substring(0, 5));
+            PTE entry = pageTable.get(pageNum);
+            entry.i = pageNum;
+            entry.reference = true;
+            if (line[1].equals("W")) {
+                entry.dirty = true;
+            }
+            if (!entry.v) {
+                pageFaults++;
+                if (cur < frames) {
+                    pageFrames[cur] = pageNum;
+                    entry.frame = cur;
+                    entry.v = true;
+                    cur++;
+                } else {
+                    //Evicting
+                }
+                pageTable.put(pageNum, entry);
+                memaccess++;
+            }
+        }
+            System.out.println("NRU");
+            System.out.println("Number of Frames: " + frames);
+            System.out.println("Total Memory Accesses: " + memaccess);
+            System.out.println("Total Page Faults " + pageFaults);
+            System.out.println("Total Writes to Disk " + writes);
+            //Ideal seems to be about 75
     }
+
 }
