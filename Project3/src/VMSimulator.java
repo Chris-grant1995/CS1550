2059a76b1800c0648d102e8df99cb3b60841361b
Chris Grant <cgrant1995@me.com>
2016-03-26 10:59:02 -0400

    Saturday, March 26, 2016 at 10:59:02 AM

--- Project3/src/VMSimulator.java
+++ Project3/src/VMSimulator.java
@@ -3,7 +3,7 @@
  */
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Scanner;
 public class VMSimulator {
@@ -11,13 +11,91 @@ public class VMSimulator {
 
     }
 
-    public void opt(int frames, String tracefile){
+    public void opt(int frames, String tracefile) throws FileNotFoundException{
         int memaccess =0;
         int pageFaults = 0;
         int writes =0;
         int[] pageFrames = new int[frames];
 
+        HashMap<Integer, PTE> pageTable = new HashMap<>();
+        HashMap<Integer, LinkedList<Integer>> future = new HashMap<>();
+        Scanner scan = new  Scanner(new File(tracefile));
+        for(int i =0; i< 1024*1024; i++){
+            PTE temp = new PTE();
+            pageTable.put(i,temp);
+            future.put(i, new LinkedList<Integer>());
+        }
+        for(int i = 0; i<frames; i++){
+            pageFrames[i] = -1;
+        }
+        int lineNum =0;
+        while(scan.hasNext()){
+            String[] line = scan.nextLine().split(" ");
+            int pageNum = Integer.decode("0x" + line[0].substring(0,5)); // 0,5 Because our page table doesn't support 8 hex digits worth of addresses
+            future.get(pageNum).add(lineNum);
+            lineNum++;
+        }
+        int cur = 0;
+        scan = new Scanner(new File(tracefile));
+        while(scan.hasNext()){
+            String[] line = scan.nextLine().split(" ");
+            int pageNum = Integer.decode("0x" + line[0].substring(0,5)); // 0,5 Because our page table doesn't support 8 hex digits worth of addresses
+            future.get(pageNum).removeFirst();
+            PTE entry = pageTable.get(pageNum);
+            entry.i = pageNum;
+            entry.reference = true;
+            if(line[1].equals("W")){
+                entry.dirty = true;
+            }
+            if(!entry.v){
+                pageFaults++;
+                if(cur<frames){
+                    //No eviction
+                    pageFrames[cur] = pageNum;
+                    entry.frame = cur;
+                    entry.v = true;
+                    cur++;
+                }
+                else{
+                    //Evicting
+                    int maxDist = getLongestDist(pageFrames, future);
+                    PTE evict = pageTable.get(maxDist);
+                    if(entry.dirty){
+                        writes++;
+                    }
+                    pageFrames[evict.frame] = entry.i;
+                    entry.frame = evict.frame;
+                    entry.v = true;
+                    evict.dirty = false;
+                    evict.reference = false;
+                    evict.v = false;
+                    evict.frame = -1;
+                    pageTable.put(maxDist,entry);
 
+                }
+            }
+            pageTable.put(pageNum,entry);
+            memaccess++;
+        }
+        System.out.println("Opt");
+        System.out.println("Number of Frames: "+ frames);
+        System.out.println("Total Memory Accesses: "+ memaccess);
+        System.out.println("Total Page Faults "+ pageFaults);
+        System.out.println("Total Writes to Disk "+ writes);
+    }
+    public int getLongestDist(int [] pageFrames, HashMap<Integer, LinkedList<Integer>> future ){
+        int index =0;
+        int maxDist = 0;
+        for(int i =0; i<pageFrames.length; i++){
+            if (future.get(pageFrames[i]).isEmpty()){
+                return pageFrames[i];
+            }
+            else if(future.get(pageFrames[i]).get(0) > maxDist){
+                maxDist = future.get(pageFrames[i]).get(0);
+                index = pageFrames[i];
+            }
+        }
+        return index;
     }
     public void clock(int frames, String tracefile)throws FileNotFoundException{
         int memaccess =0;
@@ -209,6 +287,7 @@ public class VMSimulator {
         System.out.println("Total Memory Accesses: "+ memaccess);
         System.out.println("Total Page Faults "+ pageFaults);
         System.out.println("Total Writes to Disk "+ writes);
+        //Ideal seems to be about 75
 
     }
     public void aging(int frames, String tracefile, int refresh){
