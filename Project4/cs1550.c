d3d39a6174225396395b4e6797f777e17f46236a
Chris Grant <cgrant1995@me.com>
2016-04-13 16:52:34 -0400

    Added ReadDir

--- Project4/cs1550.c
+++ Project4/cs1550.c
@@ -44,7 +44,7 @@ struct cs1550_directory_entry
 	} __attribute__((packed)) files[MAX_FILES_IN_DIR];	//There is an array of these
 
 	//This is some space to get this to be exactly the size of the disk block.
-	//Don't use it for anything.  
+	//Don't use it for anything.
 	char padding[BLOCK_SIZE - MAX_FILES_IN_DIR * sizeof(struct cs1550_file_directory) - sizeof(int)];
 } ;
 
@@ -63,7 +63,7 @@ struct cs1550_root_directory
 	} __attribute__((packed)) directories[MAX_DIRS_IN_ROOT];	//There is an array of these
 
 	//This is some space to get this to be exactly the size of the disk block.
-	//Don't use it for anything.  
+	//Don't use it for anything.
 	char padding[BLOCK_SIZE - MAX_DIRS_IN_ROOT * sizeof(struct cs1550_directory) - sizeof(int)];
 } ;
 
@@ -71,7 +71,7 @@ typedef struct cs1550_root_directory cs1550_root_directory;
 
 struct cs1550_disk_block
 {
-	//The first 4 bytes will be the value 0xF113DA7A 
+	//The first 4 bytes will be the value 0xF113DA7A
 	unsigned long magic_number;
 	//And all the rest of the space in the block can be used for actual data
 	//storage.
@@ -97,76 +97,237 @@ typedef struct cs1550_inode cs1550_inode;
 
 /*
  * Called whenever the system wants to know the file attributes, including
- * simply whether the file exists or not. 
+ * simply whether the file exists or not.
  *
  * man -s 2 stat will show the fields of a stat structure
  */
+ //Helper Funcs
+ int get_path_type(const char *path, char *dir, char *fileName, char *ext){
+	int ret = -1;
+	if (strcmp(path, "/") == 0) 					 { ret = 0; }
+ 	if (strcmp(directory, "\0") != 0)      { ret = 1; }
+ 	if (strcmp(filename, "\0") != 0)       { ret = 2; }
+ 	if (strcmp(extension, "\0") != 0)      { ret = 3; }
+ 	return ret;
+ }
+ static void get_root(cs1550_root_directory *r) {
+ 	FILE *f = fopen(".disk", "rb");
+ 	if (f != NULL) {
+ 		fread(r, sizeof(cs1550_root_directory), 1, f);
+ 		fclose(f);
+ 	}
+ }
+ static void getDir(cs1550_directory_entry *fill, char *dir) {
+ 	long startBlock = 0;
+ 	cs1550_root_directory r;
+ 	get_root(&r);
+ 	int i;
+ 	for (i = 0; i < r.nDirectories; i++) {
+ 		if (strcmp(dir, r.directories[i].dname) == 0) {
+ 			startBlock = r.directories[i].nStartBlock;
+ 		}
+ 	}
+
+ 	FILE *f = fopen(".disk", "rb");
+ 	if (f != NULL) {
+ 		// set the fill directory entry passed in to the directory we found
+ 		fseek(f, startBlock, SEEK_SET);
+ 		fread(fill, BLOCK_SIZE, 1, f);
+ 		fclose(f);
+ 	}
+ }
+ static int dirExists(char *dir) {
+ 	int ret = 0;
+ 	cs1550_root_directory r;
+ 	get_root(&r);
+ 	int i;
+ 	for (i = 0; i < r.nDirectories; i++) {
+ 		if (strcmp(dir, r.directories[i].dname) == 0) {
+ 			ret = 1;
+ 		}
+ 	}
+ 	return ret;
+ }
+ static int fileExists(char *dir, char *fileName, char *ext, int pathType) {
+ 	int ret = -1;
+
+ 	if (dirExists(dir) == 0) {
+ 		ret = -1;
+ 	} else {
+ 		cs1550_directory_entry parentDir;
+ 		getDir(&parentDir, directory);
+
+ 		int i;
+ 		for (i = 0; i < parentDir.nFiles; i++) {
+ 			if (pathType == 2 && strcmp(fileName, parentDir.files[i].fname) == 0) {
+ 				ret = (int)parentDir.files[i].fsize;
+ 			}
+			else if (pathType == 3 && strcmp(fileName, parentDir.files[i].fname) == 0 && strcmp(ext, parentDir.files[i].fext) == 0 ) {
+ 					ret = (int)parentDir.files[i].fsize;
+ 			}
+ 		}
+ 	}
+ 	return ret;
+ }
 static int cs1550_getattr(const char *path, struct stat *stbuf)
 {
 	int res = 0;
 
 	memset(stbuf, 0, sizeof(struct stat));
-   
+
+	 char dir[MAX_FILENAME+1];
+	 char fileName[MAX_FILENAME+1];
+	 char ext = [MAX_EXTENSION+1];
+
+	 dir[0] = '\0';
+	 fileName[0] = '\0';
+	 ext[0] = '\0';
+
+	 sscanf(path, "/%[^/]/%[^.].%s", directory, filename, extension);
+
+	 dir[MAX_FILENAME] = '\0';
+	 fileName[MAX_FILENAME] = '\0';
+	 ext[MAX_EXTENSION] = '\0';
+
+
+
 	//is path the root dir?
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 	} else {
-
+		int pathType = get_path_type(path,dir,fileName, ext);
+		if(pathType == 1){
+			//subdirectory
+			if(dirExists(dir) == 1){
+				//subdirectory exists
+				stbuf->st_mode = S_IFDIR | 0755;
+				stbuf->st_nlink = 2;
+			}
+			else{
+				res = -ENOENT;
+			}
+		}
 	//Check if name is subdirectory
-	/* 
+	/*
 		//Might want to return a structure with these fields
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 		res = 0; //no error
 	*/
-
+	else if(pathType == 2 || pathType == 3){
+		//File
+		int fSize = fileExists(dir,fileName,ext,pathType);
+		if(fSize != -1){
+			stbuf->st_mode = S_IFREG | 0666;
+			stbuf->st_nlink = 1; //file links
+			stbuf->st_size = (size_t) fSize); //file size - make sure you replace with real size!
+		}
+		else{
+			res = -ENOENT;
+		}
+	}
 	//Check if name is a regular file
 	/*
 		//regular file, probably want to be read and write
-		stbuf->st_mode = S_IFREG | 0666; 
+		stbuf->st_mode = S_IFREG | 0666;
 		stbuf->st_nlink = 1; //file links
 		stbuf->st_size = 0; //file size - make sure you replace with real size!
 		res = 0; // no error
 	*/
 
-		//Else return that path doesn't exist
+	else{
 		res = -ENOENT;
+	}
+		//Else return that path doesn't exist
 	}
 	return res;
 }
 
-/* 
+/*
  * Called whenever the contents of a directory are desired. Could be from an 'ls'
  * or could even be when a user hits TAB to do autocompletion
  */
 static int cs1550_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
+	int res =0;
 	//Since we're building with -Wall (all warnings reported) we need
 	//to "use" every parameter, so let's just cast them to void to
 	//satisfy the compiler
 	(void) offset;
 	(void) fi;
+	char dir[MAX_FILENAME+1];
+	char fileName[MAX_FILENAME+1];
+	char ext = [MAX_EXTENSION+1];
 
+	dir[0] = '\0';
+	fileName[0] = '\0';
+	ext[0] = '\0';
+
+	sscanf(path, "/%[^/]/%[^.].%s", directory, filename, extension);
+
+	dir[MAX_FILENAME] = '\0';
+	fileName[MAX_FILENAME] = '\0';
+	ext[MAX_EXTENSION] = '\0';
+
+	int pathType = get_path_type(path,dir,fileName,ext);
+
+	if(pathType == 0){
+		filler(buf, ".", NULL, 0);
+		filler(buf, "..", NULL, 0);
+		cs1550_root_directory r;
+		get_root(&r);
+		int i;
+		for(i = 0; i < r.nDirectories; i++) {
+			filler(buf, r.directories[i].dname, NULL, 0);
+		}
+	}
+	else if(pathType == 1){
+		int dirExists = dirExists(dir);
+		if(dirExists == 1){
+			filler(buf, ".", NULL,0);
+			filler(buf, "..", NULL, 0);
+			cs1550_directory_entry curDir;
+			getDir(&curDir, dir);
+			for(int i =0; i<curDir.nFiles; i++){
+				if(strcmp(curDir.files[i].fext, "\0") == 0){
+					filler(buf,curDir.files[i].fname, NULL, 0);
+				}
+				else{
+					char * filenameExt = (char*) malloc(2+MAX_FILENAME + MAX_EXTENSION);// 2 for . and \0
+					strcpy(filenameExt, curDir.files[i].fname);
+					strcpy(filenameExt, ".");
+					strcpy(filenameExt, curDir.files[i].fext);
+					filler(buf,filenameExt, NULL, 0);
+				}
+			}
+		}
+		else{
+			res = -ENOENT;
+		}
+	}
+	else{
+		res = -ENOENT;
+	}
+	return res;
 	//This line assumes we have no subdirectories, need to change
-	if (strcmp(path, "/") != 0)
-	return -ENOENT;
+	//if (strcmp(path, "/") != 0)
+	//return -ENOENT;
 
 	//the filler function allows us to add entries to the listing
 	//read the fuse.h file for a description (in the ../include dir)
-	filler(buf, ".", NULL, 0);
-	filler(buf, "..", NULL, 0);
+	//filler(buf, ".", NULL, 0);
+	//filler(buf, "..", NULL, 0);
 
 	/*
 	//add the user stuff (subdirs or files)
 	//the +1 skips the leading '/' on the filenames
 	filler(buf, newpath + 1, NULL, 0);
 	*/
-	return 0;
 }
 
-/* 
+/*
  * Creates a directory. We can ignore mode since we're not dealing with
  * permissions, as long as getattr returns appropriate ones for us.
  */
@@ -178,7 +339,7 @@ static int cs1550_mkdir(const char *path, mode_t mode)
 	return 0;
 }
 
-/* 
+/*
  * Removes a directory.
  */
 static int cs1550_rmdir(const char *path)
@@ -187,7 +348,7 @@ static int cs1550_rmdir(const char *path)
     return 0;
 }
 
-/* 
+/*
  * Does the actual creation of a file. Mode and dev can be ignored.
  *
  */
@@ -209,7 +370,7 @@ static int cs1550_unlink(const char *path)
     return 0;
 }
 
-/* 
+/*
  * Read size bytes from file into buf starting from offset
  *
  */
@@ -232,11 +393,11 @@ static int cs1550_read(const char *path, char *buf, size_t size, off_t offset,
 	return size;
 }
 
-/* 
+/*
  * Write size bytes from buf into file starting from offset
  *
  */
-static int cs1550_write(const char *path, const char *buf, size_t size, 
+static int cs1550_write(const char *path, const char *buf, size_t size,
 			  off_t offset, struct fuse_file_info *fi)
 {
 	(void) buf;
@@ -261,7 +422,7 @@ static int cs1550_write(const char *path, const char *buf, size_t size,
 
 /*
  * truncate is called when a new file is created (with a 0 size) or when an
- * existing file is made shorter. We're not handling deleting files or 
+ * existing file is made shorter. We're not handling deleting files or
  * truncating existing ones, so all we need to do here is to initialize
  * the appropriate directory entry.
  *
@@ -275,7 +436,7 @@ static int cs1550_truncate(const char *path, off_t size)
 }
 
 
-/* 
+/*
  * Called when we open a file
  *
  */
@@ -290,7 +451,7 @@ static int cs1550_open(const char *path, struct fuse_file_info *fi)
 
     //It's not really necessary for this project to anything in open
 
-    /* We're not going to worry about permissions for this project, but 
+    /* We're not going to worry about permissions for this project, but
 	   if we were and we don't have them to the file we should return an error
 
         return -EACCES;
@@ -301,7 +462,7 @@ static int cs1550_open(const char *path, struct fuse_file_info *fi)
 
 /*
  * Called when close is called on a file descriptor, but because it might
- * have been dup'ed, this isn't a guarantee we won't ever need the file 
+ * have been dup'ed, this isn't a guarantee we won't ever need the file
  * again. For us, return success simply to avoid the unimplemented error
  * in the debug log.
  */
